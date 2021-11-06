7b7e5768962ea672196b8f41ea875fd1b0bc830c
Manuel <mmolina@ideasoft.biz>
2021-11-06 20:20:17 -0300

    APP and Maven Conf

---
+++ src/main/java/uy/com/banca/GPCApplication.java
@@ -0,0 +1,13 @@
+package uy.com.banca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPCApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GPCApplication.class, args);
+  }
+
+}
