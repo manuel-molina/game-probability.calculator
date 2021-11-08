6f5bc57ddac8720129c154bb0256d54ed9f55bcd
Manuel <mmolina@ideasoft.biz>
2021-11-07 22:41:07 -0300

    Ajustes al calculo de probabilidades y log

--- src/main/java/uy/com/banca/domain/GameBetNode.java
+++ src/main/java/uy/com/banca/domain/GameBetNode.java
@@ -1,30 +1,27 @@
 package uy.com.banca.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
-import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Document(collection = "gameBets")
 @JsonPropertyOrder({"id", "raceCounter", "betsForWin", "probabilityToWin", "betsForLose", "probabilityToLose", "nodeWinner", "nodeLoser"})
 public class GameBetNode {
-    @Id
-    @JsonIgnore
-    private String id;
+    private static final Logger logger = Logger.getLogger(GameBetNode.class.getName());
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     @JsonProperty("ganador")
     public GameBetNode nodeWinner;
-
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     @JsonProperty("perdedor")
     public GameBetNode nodeLoser;
-
+    @Id
+    @JsonIgnore
+    private String id;
     // número de carreras
     @JsonProperty("carreras")
     private Long raceCounter;
@@ -45,6 +42,7 @@ public class GameBetNode {
     }
 
     public GameBetNode(Double pWinBet, Long raceCounter) {
+        logger.log(Level.INFO, "new GameBetNode -> parentProbability: " + pWinBet + " raceCounter: " + raceCounter);
         this.probabilityToWin = new BigDecimal(String.valueOf(pWinBet)).setScale(3, BigDecimal.ROUND_FLOOR).doubleValue();
         this.probabilityToLose = new BigDecimal(String.valueOf(1 - pWinBet)).setScale(3, BigDecimal.ROUND_FLOOR).doubleValue();
         this.raceCounter = raceCounter;
@@ -53,32 +51,50 @@ public class GameBetNode {
         this.betsForLose = raceCounter - this.betsForWin;
     }
 
-    public GameBetNode(Double parentProbability, Long races, Long bets) {
+    public GameBetNode(Double parentProbability, Boolean isWinner, Long bets) {
+        logger.log(Level.INFO, "new GameBetNode -> parentProbability: " + parentProbability + " isWinner: " + isWinner + " bets: " + bets);
         this.raceCounter = bets;
         if (bets == 1) {
-            this.betsForWin = 1;
-            this.betsForLose = 0;
+            this.betsForWin = isWinner ? 1 : 0;
+            this.betsForLose = isWinner ? 0 : 1;
         } else {
-            this.betsForWin = Math.round(parentProbability * bets);
-            this.betsForLose = this.raceCounter - this.betsForWin;
+            if (isWinner) {
+                this.betsForWin = new BigDecimal(String.valueOf(parentProbability * bets)).setScale(0, BigDecimal.ROUND_FLOOR).longValue();
+                this.betsForWin = this.betsForWin == 0d ? 1 : this.betsForWin;
+                this.betsForLose = this.raceCounter - this.betsForWin;
+            } else {
+                this.betsForLose = new BigDecimal(String.valueOf(parentProbability * bets)).setScale(0, BigDecimal.ROUND_FLOOR).longValue();
+                this.betsForLose = this.betsForLose == 0d ? 1 : this.betsForLose;
+                this.betsForWin = this.raceCounter - this.betsForLose;
+            }
         }
 
-        Double winLevelProbability = parentProbability * (this.betsForWin / this.raceCounter.doubleValue());
-        Double loseLevelProbability = parentProbability * (this.betsForLose / this.raceCounter.doubleValue());
+        if (this.raceCounter != 0) {
+            Double winLevelProbability = parentProbability * (this.betsForWin / this.raceCounter.doubleValue());
+            Double loseLevelProbability = parentProbability * (this.betsForLose / this.raceCounter.doubleValue());
 
-        this.probabilityToWin = new BigDecimal(String.valueOf(winLevelProbability)).setScale(3, BigDecimal.ROUND_FLOOR).doubleValue();
-        this.probabilityToLose = new BigDecimal(String.valueOf(loseLevelProbability)).setScale(3, BigDecimal.ROUND_FLOOR).doubleValue();
+            this.probabilityToWin = new BigDecimal(String.valueOf(winLevelProbability)).setScale(3, BigDecimal.ROUND_FLOOR).doubleValue();
+            this.probabilityToLose = new BigDecimal(String.valueOf(loseLevelProbability)).setScale(3, BigDecimal.ROUND_FLOOR).doubleValue();
+        } else {
+            this.probabilityToWin = 0d;
+            this.probabilityToLose = 0d;
+        }
     }
 
     public void generateChildRaceBets() {
+        logger.log(Level.INFO, "generateChildRaceBets -> raceCounter: " + raceCounter + " betsForWin: " + betsForWin + " betsForLose: " + betsForLose);
         if (this.raceCounter > 1) {
             // Winner levels
-            this.nodeWinner = new GameBetNode(this.probabilityToWin, this.raceCounter - 1, this.betsForWin);
-            this.nodeWinner.generateChildRaceBets();
+            if (this.betsForWin > 0) {
+                this.nodeWinner = new GameBetNode(this.probabilityToWin, true, this.betsForWin);
+                this.nodeWinner.generateChildRaceBets();
+            }
 
             // Looser levels
-            this.nodeLoser = new GameBetNode(this.probabilityToLose, this.raceCounter, this.betsForLose);
-            this.nodeLoser.generateChildRaceBets();
+            if (this.betsForLose > 0) {
+                this.nodeLoser = new GameBetNode(this.probabilityToLose, false, this.betsForLose);
+                this.nodeLoser.generateChildRaceBets();
+            }
         }
     }
 
@@ -91,8 +107,8 @@ public class GameBetNode {
     private void print(StringBuilder buffer, String prefix, String childrenPrefix) {
         buffer.append(prefix);
         buffer.append(raceCounter)
-              .append(" G[").append(betsForWin).append(": ").append(probabilityToWin)
-              .append("] P[").append(betsForLose).append(": ").append(probabilityToLose).append("]");
+            .append(" G[").append(betsForWin).append(": ").append(probabilityToWin)
+            .append("] P[").append(betsForLose).append(": ").append(probabilityToLose).append("]");
         buffer.append('\n');
         if (nodeWinner != null) {
             nodeWinner.print(buffer, childrenPrefix + "├── ", childrenPrefix + "│   ");
@@ -150,6 +166,7 @@ public class GameBetNode {
         this.betsForLose = betsForLose;
     }
 
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_FLOAT, pattern = "###0.0000")
     public Double getProbabilityToWin() {
         return probabilityToWin;
     }
@@ -158,6 +175,7 @@ public class GameBetNode {
         this.probabilityToWin = probabilityToWin;
     }
 
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_FLOAT, pattern = "###0.0000")
     public Double getProbabilityToLose() {
         return probabilityToLose;
     }
